0d9f3f9343762d07e9db2d40d9b1ea0cd192af05
CarlaCapelli <92063946+CarlaCapelli@users.noreply.github.com>
2021-11-04 12:31:18 -0300

    agregaPosicion

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ let numero = Number(prompt("cuantos nombres quiere ingresar"));
 let nombre: string[] = new Array(numero);
 
 for (let i: number = 0; i < numero; i++) {
-  let nom = String(prompt("ingrese un nombre"));
+  let nom = String(prompt("ingrese un nombre para la posicion ", i));
   nombre[i] = nom;
-  console.log(nombre[i]);
+  console.log(" el nombre ingresado en la posicion ", i, " es ", nombre[i]);
 }
